509984c039bd58ceee647c162f57f9c87b33297e
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-27 00:28:58 +0600

    complete my project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/HEADER/Header';
+import Shop from './components/Shop/Shop';
+import Product from './components/Product/Product';
 
 function App() {
   return (
     <div>
       <Header></Header>
+       <Shop></Shop>
        
       
     </div>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart)
+    //const total = cart.reduce((total, prd) => total + prd.price , 0 )
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+    }
+    let shipping = 0;
+    if (total > 35) {
+        shipping = 0;
+    }
+
+    if (total > 15) {
+        shipping = 5.00;
+    }
+    else if (total > 0) {
+        shipping = 12.90
+    }
+
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+    const formateNumber = num => {
+        const pricision = num.toFixed(2);
+        return Number(pricision);
+    }
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered : {cart.length}</p>
+            <p>product price:{formateNumber(total)}</p>
+
+            <p><small>Shipping Cost :{shipping}</small></p>
+            <p><small>Tax+VaT:{tax}</small></p>
+            <p>Total Price:{grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
