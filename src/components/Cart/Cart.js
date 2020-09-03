3b0b59ffd8d2f0c65126bafee263bb9068555bd8
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-09-03 16:45:17 +0600

    processing

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart)
+   
+    //console.log(cart);
     //const total = cart.reduce((total, prd) => total + prd.price , 0 )
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price*product.quantity;
+        
     }
     let shipping = 0;
     if (total > 35) {
@@ -29,13 +32,17 @@ const Cart = (props) => {
     }
     return (
         <div>
-            <h4>Order Summary</h4>
+            <h4 className="text-primary">Order Summary</h4>
             <p>Items Ordered : {cart.length}</p>
             <p>product price:{formateNumber(total)}</p>
 
             <p><small>Shipping Cost :{shipping}</small></p>
             <p><small>Tax+VaT:{tax}</small></p>
             <p>Total Price:{grandTotal}</p>
+            <br />
+            <Link to='/review'>
+            <button className='cart-btn'>Review Order</button>
+            </Link>
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,27 +2,28 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     //console.log(props);
-    const { img, name, seller, price, stock } = props.product;
+    const { img, name, seller, price, stock, key } = props.product;
     return (
         <div className='product'>
             <div>
                 <img src={img} alt="" />
             </div>
             <div>
-                <h4 className='product-name'>{name}</h4>
+                <h4 className='product-name'><Link to={'/' + key}>{name}</Link></h4>
                 <br />
                 <p><small>by:{seller} </small></p>
                 <p>$:{price}</p>
                 <br />
                 <p><small>only{stock}left in stock-order soon </small></p>
-                <button 
-                className='cart-btn'
-                onClick= {() => props.handleAddProduct(props.product)}
-                > 
-                <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+                {props.showAddToCart === true && <button
+                    className='cart-btn'
+                    onClick={() => props.handleAddProduct(props.product)}
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>}
 
 
             </div>
